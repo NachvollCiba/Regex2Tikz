80504edecf3e0e2171fb58008b2ab4381397769b
Dennis Ciba <dennis.ciba@uncompilable.com>
2015-12-13 14:19:40 +0100

    Implemented grid drawing for the automaton canvas

--- autCanvas.js
+++ autCanvas.js
@@ -6,6 +6,7 @@
 const ZOOM_STEP = 1;
 const ZOOM_MAX = 100;
 const STATE_RAD = .5;
+const SNAP_RAD = .25;
 
 function CanvasController(canvas, automaton, controlElem) {
     // class member
@@ -14,11 +15,26 @@ function CanvasController(canvas, automaton, controlElem) {
     this.control = controlElem;
     this.camera = {zoom:40, x:this.canvas[0].width/2, y:this.canvas[0].height/2};
     this.changelistener = null;
+    this.spatial = {xMap: new Map(), yMap: new Map()};
+    this.moving = null;
+    this.xAligned = null;
+    this.yAligned = null;
+    this.showGrid = false;
 
     // class methods
+
+    this.initialize = function() {
+        // build the spatial data structure
+        this.buildSpatial();
+    };
+
     this.drawAutomaton = function() {
         var ctx = this.canvas[0].getContext("2d");
         ctx.clearRect(0, 0, this.canvas[0].width, this.canvas[0].height);
+
+        if (this.showGrid) {
+            this.drawGrid(ctx);
+        }
 
         var fontsize = .5 * this.camera.zoom;
         ctx.font = fontsize + "px Arial";
@@ -31,9 +47,47 @@ function CanvasController(canvas, automaton, controlElem) {
             var posX =  state.position[0] * this.camera.zoom + this.camera.x;
             var posY = -state.position[1] * this.camera.zoom + this.camera.y;
 
-            ctx.beginPath();
-            ctx.arc(posX, posY, realRad, 0, 2 * Math.PI);
-            ctx.stroke();
+
+            if (state == this.moving) {
+                ctx.beginPath();
+                ctx.arc(posX, posY, realRad, 0, 2 * Math.PI);
+                ctx.fillStyle = "#e5ecff";
+                ctx.fill();
+                ctx.stroke();
+                ctx.fillStyle = "#000000";
+
+                if (this.xAligned != null) {
+                    var otherX =  this.xAligned.position[0] * this.camera.zoom + this.camera.x;
+                    var otherY = -this.xAligned.position[1] * this.camera.zoom + this.camera.y;
+
+                    ctx.beginPath();
+                    ctx.strokeStyle = "#A52A2A";
+                    ctx.moveTo(posX, posY);
+                    ctx.lineTo(otherX, otherY);
+                    ctx.stroke();
+                    ctx.strokeStyle = "#000000";
+                }
+
+                if (this.yAligned != null) {
+                    var otherX =  this.yAligned.position[0] * this.camera.zoom + this.camera.x;
+                    var otherY = -this.yAligned.position[1] * this.camera.zoom + this.camera.y;
+
+                    ctx.beginPath();
+                    ctx.strokeStyle = "#A52A2A";
+                    ctx.moveTo(posX, posY);
+                    ctx.lineTo(otherX, otherY);
+                    ctx.stroke();
+                    ctx.strokeStyle = "#000000";
+                }
+
+            } else {
+                ctx.beginPath();
+                ctx.arc(posX, posY, realRad, 0, 2 * Math.PI);
+                ctx.fillStyle = "#f2f2f2";
+                ctx.fill();
+                ctx.stroke();
+                ctx.fillStyle = "#000000";
+            }
 
             ctx.fillText(toInternalID(state.name), posX, posY);
 
@@ -49,18 +103,63 @@ function CanvasController(canvas, automaton, controlElem) {
                     var startX = posX + realRad * dir[0]; var startY = posY - realRad * dir[1];
                     var endX = otherX - realRad * dir[0]; var endY = otherY + realRad * dir[1];
 
+                    ctx.beginPath();
                     ctx.moveTo(startX, startY);
                     ctx.lineTo(endX, endY);
                     ctx.stroke();
 
-                    var midX = (posX + (otherX - posX) / 2);
-                    var midY = (posY + (otherY - posY) / 2);
-                    ctx.moveTo(midX, midY);
+                    //var midX = (posX + (otherX - posX) / 2);
+                    //var midY = (posY + (otherY - posY) / 2);
+                    //ctx.moveTo(midX, midY);
                     //symb = symb == EPS ? "€" : symb;
                     //ctx.fillText(symb, midX, midY);
                 }
             }
         }
+    };
+
+    this.drawGrid = function(ctx) {
+        var width = this.canvas[0].width; var height = this.canvas[0].height;
+        var stepSize = 2* SNAP_RAD * this.camera.zoom;
+
+
+
+        var startX = this.camera.x - Math.floor(this.camera.x / (stepSize)) * stepSize;
+        var startY = this.camera.y - Math.floor(this.camera.y / (stepSize)) * stepSize;
+        console.log(startX);
+
+        // draw major lines
+        ctx.beginPath();
+        ctx.strokeStyle = "#000000";
+
+        for (var x = startX; x <= width; x+= stepSize) {
+            ctx.moveTo(x, 0);
+            ctx.lineTo(x, height);
+        }
+
+        for (var y = startY; y <= height; y+= stepSize) {
+            ctx.moveTo(0, y);
+            ctx.lineTo(width, y);
+        }
+
+        ctx.stroke();
+
+        // draw minor lines
+        ctx.beginPath();
+        ctx.strokeStyle = "#bfbfbf";
+
+        for (x = startX - stepSize / 2; x <= width; x+= stepSize) {
+            ctx.moveTo(x, 0);
+            ctx.lineTo(x, height);
+        }
+
+        for (y = startY - stepSize / 2; y <= height; y+= stepSize) {
+            ctx.moveTo(0, y);
+            ctx.lineTo(width, y);
+        }
+
+        ctx.stroke();
+        ctx.strokeStyle = "#000000";
     };
 
     this.zoomIn = function(amount) {
@@ -76,6 +175,132 @@ function CanvasController(canvas, automaton, controlElem) {
     };
 
 
+    this.spatialIndex = function(state) {
+        var s = SNAP_RAD;
+
+        var x1 = 2 * Math.floor(state.position[0] / (2*s));
+        var x2 = 2 * Math.floor((state.position[0] + s) / (2*s)) - 1;
+
+        var y1 = 2 * Math.floor(state.position[1] / (2*s));
+        var y2 = 2 * Math.floor((state.position[1] + s) / (2*s)) - 1;
+
+        return {x: [x1, x2], y: [y1, y2]};
+    };
+
+    this.addToSpatial = function(state, map, idx) {
+        if (!map.has(idx)) {
+            map.set(idx, new Set());
+        }
+        map.get(idx).add(state);
+
+    };
+
+    this.updateStateSpatial = function(state) {
+        var spatialIdx = this.spatialIndex(state);
+        var changed = false;
+
+        if (state.spatial == null) {
+            state.spatial = spatialIdx;
+            this.addToSpatial(state, this.spatial.xMap, spatialIdx.x[0]);
+            this.addToSpatial(state, this.spatial.xMap, spatialIdx.x[1]);
+
+            this.addToSpatial(state, this.spatial.yMap, spatialIdx.y[0]);
+            this.addToSpatial(state, this.spatial.yMap, spatialIdx.y[1]);
+
+            changed = true;
+        } else {
+            if (spatialIdx.x[0] != state.spatial.x[0]) {
+                this.spatial.xMap.get(state.spatial.x[0]).delete(state);
+                this.addToSpatial(state, this.spatial.xMap, spatialIdx.x[0]);
+                changed = true;
+            }
+            if (spatialIdx.x[1] != state.spatial.x[1]) {
+                this.spatial.xMap.get(state.spatial.x[1]).delete(state);
+                this.addToSpatial(state, this.spatial.xMap, spatialIdx.x[1]);
+                changed = true;
+            }
+            if (spatialIdx.y[0] != state.spatial.y[0]) {
+                this.spatial.yMap.get(state.spatial.y[0]).delete(state);
+                this.addToSpatial(state, this.spatial.yMap, spatialIdx.y[0]);
+                changed = true;
+            }
+            if (spatialIdx.y[1] != state.spatial.y[1]) {
+                this.spatial.yMap.get(state.spatial.y[1]).delete(state);
+                this.addToSpatial(state, this.spatial.yMap, spatialIdx.y[1]);
+                changed = true;
+            }
+        }
+
+        state.spatial = spatialIdx;
+        return changed;
+    };
+
+    this.findMinDisStateSameCol = function(state) {
+        var x1 = state.spatial.x[0]; var x2 = state.spatial.x[1];
+        var minDis = Infinity;
+        var minState = null;
+
+        this.spatial.xMap.get(x1).forEach(function(other) {
+            if (other != state) {
+                var dis = Math.abs(state.position[1] - other.position[1]);
+
+                if (dis < minDis) {
+                    minDis = dis;
+                    minState = other;
+                }
+            }
+        });
+
+        this.spatial.xMap.get(x2).forEach(function(other) {
+            if (other != state) {
+                var dis = Math.abs(state.position[1] - other.position[1]);
+
+                if (dis < minDis) {
+                    minDis = dis;
+                    minState = other;
+                }
+            }
+        });
+
+        return minState;
+    };
+
+    this.findMinDisStateSameRow = function(state) {
+        var y1 = state.spatial.y[0]; var y2 = state.spatial.y[1];
+        var minDis = Infinity;
+        var minState = null;
+
+        this.spatial.yMap.get(y1).forEach(function(other) {
+            if (other != state) {
+                var dis = Math.abs(state.position[0] - other.position[0]);
+
+                if (dis < minDis) {
+                    minDis = dis;
+                    minState = other;
+                }
+            }
+        });
+
+        this.spatial.yMap.get(y2).forEach(function(other) {
+            if (other != state) {
+                var dis = Math.abs(state.position[0] - other.position[0]);
+
+                if (dis < minDis) {
+                    minDis = dis;
+                    minState = other;
+                }
+            }
+        });
+
+        return minState;
+    };
+
+    this.buildSpatial = function() {
+        for (var i = 0; i < this.automaton.length; i++) {
+            this.updateStateSpatial(this.automaton[i]);
+        }
+    };
+
     // listener
     var cntrl = this;
     var selected = null;
@@ -88,7 +313,12 @@ function CanvasController(canvas, automaton, controlElem) {
             cntrl.changelistener(selected);
         }
 
+        cntrl.moving = null;
+        cntrl.xAligned = null;
+        cntrl.yAligned = null;
+
         selected = null;
+        cntrl.drawAutomaton();
     };
 
     this.canvasMouseDown = function(event) {
@@ -112,6 +342,8 @@ function CanvasController(canvas, automaton, controlElem) {
         }
     };
 
+    var locked = {x: false, y:false};
+    var accDelta = {x:0, y:0};
     this.canvasMouseMove = function(event) {
         event.preventDefault();
 
@@ -120,9 +352,52 @@ function CanvasController(canvas, automaton, controlElem) {
             var deltaX = event.offsetX - lastMouse.x;
             var deltaY = event.offsetY - lastMouse.y;
 
-            if (selected instanceof State) { // translate the selected state
-                selected.position[0] += deltaX / cntrl.camera.zoom;
-                selected.position[1] -= deltaY / cntrl.camera.zoom;
+            if (selected instanceof State) {
+                cntrl.moving = selected;
+
+                // translate the selected state
+                var movX = deltaX / cntrl.camera.zoom;
+                var movY = deltaY / cntrl.camera.zoom;
+
+                if (locked.x) {
+                    accDelta.x += movX;
+                    console.log(locked);
+                    console.log(accDelta);
+                    if (Math.abs(accDelta.x) > SNAP_RAD) {
+                        console.log(selected.position);
+                        selected.position[0] += accDelta.x;
+                        console.log(selected.position);
+                        accDelta.x = 0;
+                    }
+                } else {
+                    selected.position[0] += movX;
+                }
+
+                if (locked.y) {
+                    accDelta.y += movY;
+                    if (Math.abs(accDelta.y) > SNAP_RAD) {
+                        selected.position[1] += accDelta.y;
+                        accDelta.y = 0;
+                    } else {
+                        selected.position[1] += movY;
+                    }
+                }
+
+                // update aligned states
+                if (cntrl.updateStateSpatial(selected)) {
+                    cntrl.xAligned = cntrl.findMinDisStateSameRow(selected);
+                    cntrl.yAligned = cntrl.findMinDisStateSameCol(selected);
+
+                    if (cntrl.xAligned != null) {
+                        selected.position[1] = cntrl.xAligned.position[1];
+                    }
+
+                    if (cntrl.yAligned != null) {
+                        selected.position[0] = cntrl.yAligned.position[0];
+                    }
+
+                    locked = {x: cntrl.yAligned != null, y: cntrl.xAligned != null};
+                }
 
                 // TODO live editing of the tikz code
             } else { // move the camera
@@ -171,4 +446,8 @@ function CanvasController(canvas, automaton, controlElem) {
         cntrl.zoomOut(3*ZOOM_STEP);
     });
 
+    this.control.find("#cbGrid").click(function(event) {
+        cntrl.showGrid = cntrl.control.find("#cbGrid").is(":checked");
+        cntrl.drawAutomaton();
+    });
 }
